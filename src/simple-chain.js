4388fa488f6b3ec99497727a997bd45e94080ba8
Svetlana <svetochkaufo@yandex.ru>
2020-02-24 16:01:13 +0300

    feat: add solution simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,41 @@
 const chainMaker = {
+  arr: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.arr.length;
   },
+
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof value == 'function') {
+      value = new String(value).toString().replace(/ +/, '');
+      this.arr.push(value);
+    } else {
+      this.arr.push(value);
+    }
+      return this;
   },
+
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+     if (typeof position === 'number' && position < this.arr.length && position > 0) {
+       this.arr.splice(position-1, 1);
+       return this;
+     }
+       else {
+         this.arr = [];
+         throw new Error('Error');
+     }
   },
+
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
+
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let newStr = this.arr.reduce((str, item) => str +'( ' + new String(item).toString() + ' )~~', '');
+    console.log(newStr.substring(0, newStr.length - 2));
+    this.arr = [];
+    return newStr.substring(0, newStr.length - 2);
   }
 };
 
