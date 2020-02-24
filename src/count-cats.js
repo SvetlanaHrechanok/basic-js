e33112211bd923b33a1c5a1879a8a0a4681503cb
Svetlana <svetochkaufo@yandex.ru>
2020-02-24 16:06:11 +0300

    fix: correct one mistake

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,5 @@ module.exports = function countCats(matrix) {
   let arr = [].concat(...matrix);
   let numberCats = arr.filter(item => item == "^^").length;
 
-  throw 'Not implemented';
-
   return numberCats;
 };
